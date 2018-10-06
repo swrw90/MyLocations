d223ff42a766cff86e28134e31628f69821a33f4
Seth Watson <seth.watson1@gmail.com>
2018-10-05 20:14:57 -0600

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Seth Watson on 10/5/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
