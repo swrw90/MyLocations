24129186fb1bfdf7daa358f5d1cc0e8227052879
Seth Watson <seth.watson1@gmail.com>
2018-10-12 15:25:08 -0600

    added tableView setup functions

--- MyLocations/CategoryPickerViewController.swift
+++ MyLocations/CategoryPickerViewController.swift
@@ -32,4 +32,32 @@ class CategoryPickerViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let categoryName = categories[indexPath.row]
+        cell.textLabel!.text = categoryName
+        
+        if categoryName == selectedCategoryName {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row != selectedIndexPath.row {
+            if let newCell = tableView.cellForRow(at: indexPath) {
+                newCell.accessoryType = .checkmark
+            }
+            if let oldCell = tableView.cellForRow(at: selectedIndexPath) {
+                oldCell.accessoryType = .none
+            }
+            selectedIndexPath = indexPath
+        }
+    }
+    
+    
 }
