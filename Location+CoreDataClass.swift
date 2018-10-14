1bd657a253f3ec5e888ab67086deffa17e0a4811
Seth Watson <seth.watson1@gmail.com>
2018-10-13 23:53:35 -0600

    modified location data model

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Seth Watson on 10/13/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ Location+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocations
+//
+//  Created by Seth Watson on 10/13/18.
+//  Copyright © 2018 Seth Watson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var category: String
+    @NSManaged var date: Date
+    @NSManaged public var latitude: Double
+    @NSManaged public var locationDescription: String
+    @NSManaged public var longitude: Double
+    @NSManaged var placemark: CLPlacemark?
+
+}
