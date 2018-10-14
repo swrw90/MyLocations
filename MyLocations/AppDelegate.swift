3db17212a12a0e4282a8087f2e92bf78a88ae65a
Seth Watson <seth.watson1@gmail.com>
2018-10-14 13:31:50 -0600

    added persistentContainer property to AppDelegate  class

--- MyLocations/AppDelegate.swift
+++ MyLocations/AppDelegate.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: {
+        storeDescription, error in
+        if let error = error {
+        fatalError("Could load data store: \(error)")
+        }
+        })
+        return container
+        }()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
