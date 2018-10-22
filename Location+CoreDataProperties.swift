bcb2ae68c478cf2bece14f5d1418c61cf7a90553
Seth Watson <seth.watson1@gmail.com>
2018-10-22 16:28:56 -0600

    modified locations core data

--- Location+CoreDataProperties.swift
+++ Location+CoreDataProperties.swift
@@ -23,5 +23,28 @@ extension Location {
     @NSManaged public var locationDescription: String
     @NSManaged public var longitude: Double
     @NSManaged var placemark: CLPlacemark?
-
+    @NSManaged public var photoID: NSNumber?
+    
+    var hasPhoto: Bool {
+        return photoID != nil
+        
+    }
+    
+    var photoURL: URL {
+        assert(photoID != nil, "No photo ID set")
+        let filename = "Photo-\(photoID!.intValue).jpg"
+        return applicationDocumentDirectory.appendingPathComponent(filename)
+    }
+    
+    var photoImage: UIImage? {
+        return UIImage(contentsOfFile: photoURL.path)
+    }
+    
+    class func nextPhotoID() -> Int {
+        let userDefaults = UserDefaults.standard
+        let currentID = userDefaults.integer(forKey: "PhotoID") + 1
+        userDefaults.set(currentID, forKey: "PhotoID")
+        userDefaults.synchronize()
+        return currentID
+    }
 }
