d5c542681b090520f72d2c110aadfbf005320172
Seth Watson <seth.watson1@gmail.com>
2018-10-23 18:47:59 -0600

    changed place mark formatting in configure of LocationCell

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -23,17 +23,11 @@ class LocationCell: UITableViewCell {
             descriptionLabel.text = location.locationDescription
         }
         
-        if let placemark = location.placemark {
+          if let placemark = location.placemark {
             var text = ""
-            if let s = placemark.subThoroughfare {
-                text += s + " "
-            }
-            if let s = placemark.thoroughfare {
-                text += s + ", "
-            }
-            if let s = placemark.locality {
-                text += s
-            }
+            text.add(text: placemark.subThoroughfare)
+            text.add(text: placemark.thoroughfare, separatedBy: " ")
+            text.add(text: placemark.locality, separatedBy: ", ")
             addressLabel.text = text
         } else {
             addressLabel.text = String(format: "Lat: %.8f", location.latitude, location.longitude)
