5242a84fb18f85d380be7f2cece373a366c9555f
Seth Watson <seth.watson1@gmail.com>
2018-10-23 23:27:54 -0600

    added more styling, shaped images, all caps headers

--- MyLocations/LocationCell.swift
+++ MyLocations/LocationCell.swift
@@ -41,6 +41,11 @@ class LocationCell: UITableViewCell {
         let selection = UIView(frame: CGRect.zero)
         selection.backgroundColor = UIColor(white: 1.0, alpha: 0.2)
         selectedBackgroundView = selection
+        
+        // Rounded corners for images
+        photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
+        photoImageView.clipsToBounds = true
+        separatorInset = UIEdgeInsets(top: 0, left: 82, bottom: 0, right: 0)
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -53,7 +58,7 @@ class LocationCell: UITableViewCell {
         if location.hasPhoto, let image = location.photoImage {
         return image.resized(withBounds: CGSize(width: 52, height: 52))
         }
-        return UIImage()
+        return UIImage(named: "No Photo")!
     }
 
 }
--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -119,7 +119,7 @@ class LocationsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let sectionInfo = fetchedResultsController.sections![section]
-        return sectionInfo.name
+        return sectionInfo.name.uppercased()
     }
     
     
